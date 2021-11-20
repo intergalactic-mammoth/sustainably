865459aa287fbbbb84a72b2b64aa49047c4c9409
Felix Köhler <f.koehler@tum.de>
2021-11-20 12:44:54 +0100

    New Method Creating the Div

--- backend-service/apis/Chart.py
+++ backend-service/apis/Chart.py
@@ -2,7 +2,6 @@
 Providing charts based on user statistics.
 """
 
-from flask import config
 from flask_restful import Resource
 import pandas as pd
 
@@ -25,19 +24,20 @@ class Chart(Resource):
             template="plotly_dark",
         )
 
-
         carbon_footprint_over_time_line_plot.update_layout(
             xaxis_title="",
             yaxis_title="Carbon Footprint",
             modebar=None,
         )
 
-        carbon_footprint_over_time_line_plot.show(
-            config={
-                "displayModeBar": False,
-                "staticPlot": True,
-            }
-        )
+        # carbon_footprint_over_time_line_plot.show(
+        #     config={
+        #         "displayModeBar": False,
+        #         "staticPlot": True,
+        #     }
+        # )
+
+        return carbon_footprint_over_time_line_plot
     
     def _item_sustainability(self, user_id):
         """
@@ -69,25 +69,65 @@ class Chart(Resource):
             textfont_size=30,
         )
 
-        item_sustainability_pie_chart.update_layout(
-            show_legend=False,
-        )
-
         return item_sustainability_pie_chart
 
-    def get(self, user_id, chart_type=None):
-        x = np.linspace(0, 2, 100)
-        y = np.sin(x)
-
-        fig = px.line(x=x, y=y)
-
-        fig_div = offline.plot(
-            figure_or_data=fig,
+    
+    def _convert_to_div(self, figure):
+        """
+        Takes a plotly figure object and turns it into a str with an html div
+        object that can then be embedded into a webpage.
+        """
+        figure_div = offline.plot(
+            figure_or_data=figure,
             include_plotlyjs=False,
             output_type="div"
         )
 
-        return fig_div
+        return figure_div
+
+
+    def get(self, user_id, chart_type=None):
+        if chart_type is None:
+            time_series_chart_div = self._convert_to_div(
+                self._carbon_footprint_over_time(
+                    user_id=user_id,
+                )
+            )
+
+            pie_chart_div = self._convert_to_div(
+                self._item_sustainability(
+                    user_id=user_id,
+                )
+            )
+
+            return_dict = {
+                "footprint_over_time": time_series_chart_div,
+                "item_sustainability": pie_chart_div,
+            }
+
+            return return_dict
+
+        elif chart_type == "footprint_over_time":
+            time_series_chart_div = self._convert_to_div(
+                self._carbon_footprint_over_time(
+                    user_id=user_id,
+                )
+            )
+
+            return time_series_chart_div
+
+        elif chart_type == "item_sustainability":
+            pie_chart_div = self._convert_to_div(
+                self._item_sustainability(
+                    user_id=user_id,
+                )
+            )
+
+            return pie_chart_div
+        
+        else:
+            raise ValueError("Requested Chart is not available")
+
 
 if __name__ == "__main__":
     chart_resource = Chart()
